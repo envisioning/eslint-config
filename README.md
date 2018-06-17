295b47c639a393c39301600c6daf52aa5f180141
lnmunhoz <ln.munhoz@gmail.com>
2018-06-17 16:05:24 +0700

    chore: Update depedencies and eslint config

--- README.md
+++ README.md
@@ -1,20 +1,23 @@
 # @envisioning/eslint-config
 
 ## Usage
-1. Install peer dependencies
+
+1.  Install peer dependencies
 
 ```bash
-yarn add -D eslint babel-eslint eslint-config-airbnb eslint-plugin-jsx-a11y eslint-plugin-react eslint-plugin-import
+yarn add -D babel-eslint@8.2.3 eslint@4.19.1 eslint-config-airbnb@16.1.0 eslint-config-prettier@2.9.0 eslint-plugin-babel@5.1.0 eslint-plugin-import@2.12.0 eslint-plugin-jest@21.17.0 eslint-plugin-jsx-a11y@6.0.3 eslint-plugin-prettier@2.6.0 eslint-plugin-react@7.9.1
 ```
 
-2. Install this package
+2.  Install this package
+
 ```bash
 yarn add -D @envisioning/eslint-config
 ```
 
-3. Add config into your .eslintrc file
+3.  Add config into your .eslintrc file
+
 ```json
 {
-  "extends": "@envisioning"
+  extends: @envisioning
 }
 ```
--- index.js
+++ index.js
@@ -1,41 +1,130 @@
 module.exports = {
+  "extends": [
+    "airbnb",
+    "prettier",
+    "prettier/react"
+  ],
   "parser": "babel-eslint",
-  "extends": "airbnb",
-  "plugins": [ "react" ],
-  "rules": {
-    "react/no-array-index-key": 0,
-    "react/prefer-stateless-function": 1,
-    "import/no-unresolved": [2, { ignore: ['meteor/*'] }],
-    "import/no-extraneous-dependencies": 0,
-    "import/no-absolute-path": 0,
-    "import/no-named-default": 0,
-    "no-unused-expressions": 0,
-    "no-underscore-dangle": 0,
-    "one-var": 0,
-    "consistent-return": 0,
-    "no-empty": 0,
-    "react/wrap-multilines": 0,
-    "jsx-a11y/img-has-alt": 0,
-    "global-require": 0,
-    "no-param-reassign": 1,
-    "import/extensions": 0,
-    "no-case-declarations": 0,
-    "react/jsx-filename-extension": [1, { "extensions": [".js", ".jsx"] }],
-    "react/require-default-props": 0,
-    "react/sort-comp": 0,
-    "import/no-unresolved": 0,
-    "import/extensions": 0,
-    "react/forbid-prop-types": 0,
-    "react/prefer-stateless-function": 0
-  },
-  "parserOptions":{
+  "parserOptions": {
+    "ecmaVersion": 8,
     "ecmaFeatures": {
       "experimentalObjectRestSpread": true,
-    },
+      "impliedStrict": true,
+      "classes": true
+    }
   },
-  globals: {
-    Package: true,
-    document: true,
-    Npm:  true,
+  "env": {
+    "browser": true,
+    "node": true,
+    "jquery": true,
+    "jest": true
   },
-};
+  "rules": {
+    "no-debugger": 0,
+    "no-alert": 0,
+    "no-unused-vars": [
+      1,
+      {
+        "argsIgnorePattern": "res|next|^err"
+      }
+    ],
+    "prefer-const": [
+      "error",
+      {
+        "destructuring": "all"
+      }
+    ],
+    "arrow-body-style": [
+      2,
+      "as-needed"
+    ],
+    "no-unused-expressions": [
+      2,
+      {
+        "allowTaggedTemplates": true
+      }
+    ],
+    "no-param-reassign": [
+      2,
+      {
+        "props": false
+      }
+    ],
+    "no-console": 0,
+    "import/prefer-default-export": 0,
+    "import": 0,
+    "func-names": 0,
+    "space-before-function-paren": 0,
+    "comma-dangle": 0,
+    "max-len": 0,
+    "import/extensions": 0,
+    "no-underscore-dangle": 0,
+    "consistent-return": 0,
+    "react/display-name": 1,
+    "react/no-array-index-key": 0,
+    "react/react-in-jsx-scope": 0,
+    "react/prefer-stateless-function": 0,
+    "react/forbid-prop-types": 0,
+    "react/no-childen-": 0,
+    "react/no-unescaped-entities": 0,
+    "jsx-a11y/accessible-emoji": 0,
+    "react/jsx-filename-extension": [
+      1,
+      {
+        "extensions": [
+          ".js",
+          ".jsx"
+        ]
+      }
+    ],
+    "radix": 0,
+    "no-shadow": [
+      2,
+      {
+        "hoist": "all",
+        "allow": [
+          "resolve",
+          "reject",
+          "done",
+          "next",
+          "err",
+          "error"
+        ]
+      }
+    ],
+    "quotes": [
+      2,
+      "single",
+      {
+        "avoidEscape": true,
+        "allowTemplateLiterals": true
+      }
+    ],
+    "prettier/prettier": [
+      "error",
+      {
+        "trailingComma": "es5",
+        "singleQuote": true,
+        "printWidth": 100
+      }
+    ],
+    "jsx-a11y/href-no-hash": "off",
+    "jsx-a11y/anchor-is-valid": [
+      "warn",
+      {
+        "aspects": [
+          "invalidHref"
+        ]
+      }
+    ],
+    "import/no-extraneous-dependencies": ["error", {
+      "devDependencies": [
+        './src/**/*.stories.js',
+        './src/**/*.test.js'
+      ]
+    }]
+  },
+  "plugins": [
+    "prettier"
+  ]
+}
